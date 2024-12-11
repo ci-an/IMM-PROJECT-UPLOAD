f2c53ec9a9eefb13f10a4c2dc79e923e5a1485ea
ci-an <b00163467@mytudublin.ie>
2024-12-11 11:51:57 +0000

    version 7

---
+++ Version 7 - Score Update/Assets/Scripts/Souls.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Souls : MonoBehaviour
+{
+    public float speed = 5f; //Up speed to up difficulty
+    private float leftEdge; //Left edge of screen
+
+    private void Start() {
+
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
+    }
+
+    private void Update() {
+
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        //If position is less than the position of the left edge, laser is destroyed. 
+        if(transform.position.x < leftEdge - 5.0f) { //Added extra length because objects were deleting before going off-screen
+            Destroy(gameObject);
+        }
+    }
+}
